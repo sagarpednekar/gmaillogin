071c1a709c5e39f7a3dbdea8082daa26549f9cbf
sagarpedenakr <sagarpedenkar04@gmail.com>
2016-08-29 02:52:29 +0530

    Login using gmail done

---
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -0,0 +1,7 @@
+class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
+  def google_oauth2
+      # You need to implement the method below in your model (e.g. app/models/user.rb)
+      @user = User.from_omniauth(request.env["omniauth.auth"])
+      sign_in_and_redirect @user
+  end
+end
